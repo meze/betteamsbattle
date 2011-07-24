8410c8a637278d758c6c3d5150ff2f657160ca25
alexidsa <alexidsa@gmail.com>
2011-07-24 09:57:57 +0000

    Setup infrastructure

---
+++ Data/Data.Model/Entities/Battle.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BetTeamsBattle.Data.Model.Entities
+{
+    public class Battle
+    {
+        public virtual long Id { get; set; }
+        public virtual DateTime StartDate { get; set; }
+
+        public virtual ICollection<User> Users { get; set; }
+    }
+}
