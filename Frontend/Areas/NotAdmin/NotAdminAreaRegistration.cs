82e942130c1785f46f19c253116c9bdd5714bb11
alexidsa <alexidsa@gmail.com>
2011-07-28 12:07:38 +0000

    Introduced areas

---
+++ Frontend/Areas/NotAdmin/NotAdminAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace BetTeamsBattle.Frontend.Areas.NotAdmin
+{
+    public class NotAdminAreaRegistration : AreaRegistration
+    {
+        public override string AreaName
+        {
+            get
+            {
+                return "NotAdmin";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            context.MapRoute(
+                "NotAdmin_default",
+                "NotAdmin/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
