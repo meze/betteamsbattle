f065ba9be60dec330c4f4b7f53d2d2d259f4565d
alexidsa <alexidsa@gmail.com>
2011-08-17 20:12:02 +0000

    Added UserStatistics entity

--- Data/Data.Model/Entities/User.cs
+++ Data/Data.Model/Entities/User.cs
@@ -17,7 +17,8 @@ namespace BetTeamsBattle.Data.Model.Entities
         public virtual UserProfile UserProfile { get; set; } 
         public virtual ICollection<BattleUser> UserBattles { get; set; }
         public virtual ICollection<BattleBet> UserBets { get; set; }
-        public virtual ICollection<BattleUserStatistics> BattlesUserStatistics { get; set; } 
+        public virtual ICollection<BattleUserStatistics> BattlesUserStatistics { get; set; }
+        public virtual UserStatistics UserStatistics { get; set; }
 
         public User()
         {
---
+++ Data/Data.Model/Entities/UserStatistics.cs
@@ -0,0 +1,11 @@
+﻿namespace BetTeamsBattle.Data.Model.Entities
+{
+    public class UserStatistics
+    {
+        public virtual long Id { get; set; }
+        public virtual long UserId { get; set; }
+        public virtual double Rating { get; set; }
+
+        public virtual User User { get; set; }
+    }
+}
