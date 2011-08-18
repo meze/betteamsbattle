923f4ecb4eb41f09b5885aecad26645b2e822e04
alexidsa <alexidsa@gmail.com>
2011-08-18 07:20:13 +0000

    Implemented UsersRating

---
+++ Frontend/Areas/NotAdmin/Controllers/UsersController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BetTeamsBattle.Data.Model.Entities;
+using BetTeamsBattle.Data.Repositories.Base.Interfaces;
+using BetTeamsBattle.Frontend.Areas.NotAdmin.ViewServices.Battles.Interfaces;
+
+namespace BetTeamsBattle.Frontend.Areas.NotAdmin.Controllers
+{
+    public partial class UsersController : Controller
+    {
+        private readonly IUsersViewService _usersViewService;
+
+        public UsersController(IUsersViewService usersViewService)
+        {
+            _usersViewService = usersViewService;
+        }
+
+        [ChildActionOnly]
+        public virtual PartialViewResult UsersRating()
+        {
+            var usersRatingUsersViewModels = _usersViewService.UsersRating();
+
+            return PartialView(usersRatingUsersViewModels);
+        }
+
+    }
+}
