70a94658a43664ae6a8d82c98db4e2903d63a361
alexidsa <alexidsa@gmail.com>
2011-08-19 03:25:46 +0000

    Got rid of markup duplication using Templated Razor Delegates

---
+++ Frontend/Models/Shared/LeftColumnBlockViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace BetTeamsBattle.Frontend.Models.Shared
+{
+    public class LeftColumnBlockViewModel
+    {
+        public string Title { get; set; }
+        public Func<dynamic, object> Content { get; set; }
+
+        public LeftColumnBlockViewModel(string title, Func<dynamic, object> content)
+        {
+            Title = title;
+            Content = content;
+        }
+    }
+}
