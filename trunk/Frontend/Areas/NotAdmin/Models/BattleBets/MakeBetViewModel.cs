8461dbe289479b470dba429c3851291ffb872557
alexidsa@gmail.com <alexidsa@gmail.com@708feb5c-fac0-84a1-9907-d073d775ba04>
2011-08-20 18:02:17 +0000

    Same

---
+++ trunk/Frontend/Areas/NotAdmin/Models/BattleBets/MakeBetViewModel.cs
@@ -0,0 +1,23 @@
+﻿namespace BetTeamsBattle.Frontend.Areas.NotAdmin.Models.BattleBets
+{
+    public class MakeBetViewModel
+    {
+        public long BattleId { get; set; }
+        public MakeBetFormViewModel MakeBetForm { get; set; }
+
+        public MakeBetViewModel(long battleId)
+        {
+            BattleId = battleId;
+            MakeBetForm = new MakeBetFormViewModel();
+        }
+    }
+
+    public class MakeBetFormViewModel
+    {
+        public string Title { get; set; }
+        public double Bet { get; set; }
+        public double Coefficient { get; set; }
+        public string Url { get; set; }
+        public bool IsPrivate { get; set; }
+    }
+}
