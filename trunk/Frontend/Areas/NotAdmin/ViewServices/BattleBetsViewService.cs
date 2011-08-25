66aa034bcd6998bd48e361cdbcce85ed6f5a47aa
alexidsa@gmail.com <alexidsa@gmail.com@708feb5c-fac0-84a1-9907-d073d775ba04>
2011-08-25 21:45:08 +0000

    Created BattleBetsService

---
+++ trunk/Frontend/Areas/NotAdmin/ViewServices/BattleBetsViewService.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using BetTeamsBattle.Data.Model.Entities;
+using BetTeamsBattle.Data.Repositories.Base.Interfaces;
+using BetTeamsBattle.Data.Repositories.Specifications;
+using BetTeamsBattle.Frontend.Areas.NotAdmin.ViewServices.Battles.Interfaces;
+
+namespace BetTeamsBattle.Frontend.Areas.NotAdmin.ViewServices
+{
+    internal class BattleBetsViewService : IBattleBetsViewService
+    {
+        private readonly IRepository<BattleBet> _repositoryOfBattleBet;
+
+        public BattleBetsViewService(IRepository<BattleBet> repositoryOfBattleBet)
+        {
+            _repositoryOfBattleBet = repositoryOfBattleBet;
+        }
+
+        public IEnumerable<BattleBet> MyBets(long battleId, long userId)
+        {
+            var myBets = _repositoryOfBattleBet.
+                   Get(BattleBetSpecifications.BattleIdAndUserIdAreEqualTo(battleId, userId)).
+                   Include(bb => bb.OpenBetScreenshot).
+                   Include(bb => bb.CloseBetScreenshot).
+                   OrderByDescending(b => b.OpenDateTime).
+                   ToList();
+
+            return myBets;
+        }
+    }
+}
---
+++ trunk/Frontend/Areas/NotAdmin/ViewServices/Interfaces/IBattleBetsViewService.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using BetTeamsBattle.Data.Model.Entities;
+
+namespace BetTeamsBattle.Frontend.Areas.NotAdmin.ViewServices.Battles.Interfaces
+{
+    public interface IBattleBetsViewService
+    {
+        IEnumerable<BattleBet> MyBets(long battleId, long userId);
+    }
+}
