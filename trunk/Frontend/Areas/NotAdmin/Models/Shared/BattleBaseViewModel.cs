a25405d7aaac784ea107b270a515fff4865ba64f
alexidsa@gmail.com <alexidsa@gmail.com@708feb5c-fac0-84a1-9907-d073d775ba04>
2011-08-26 18:39:11 +0000

    Small changes

---
+++ trunk/Frontend/Areas/NotAdmin/Models/Shared/BattleBaseViewModel.cs
@@ -0,0 +1,20 @@
+﻿namespace BetTeamsBattle.Frontend.Areas.NotAdmin.Models
+{
+    public class BattleBaseViewModel
+    {
+        public long BattleId { get; set; }
+        public string StartDate { get; set; }
+        public string EndDate { get; set; }
+        public int Budget { get; set; }
+        public int BetLimit { get; set; }
+
+        public BattleBaseViewModel(long battleId, string startDate, string endDate, int budget, int betLimit)
+        {
+            BattleId = battleId;
+            StartDate = startDate;
+            EndDate = endDate;
+            Budget = budget;
+            BetLimit = betLimit;
+        }
+    }
+}
