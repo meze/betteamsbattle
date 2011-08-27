e3d567174a2bb58bc157204b331fc480abd2c297
alexidsa <alexidsa@gmail.com>
2011-08-27 16:01:29 +0000

    Reorganized screenshots related projects

---
+++ Screenshots/AwesomiumScreenshotMaker/AwesomiumCore.cs
@@ -0,0 +1,17 @@
+﻿using AwesomiumSharp;
+
+namespace BetTeamsBattle.AwesomiumScreenshotMaker
+{
+    public class AwesomiumCore
+    {
+        public static void Initialze()
+        {
+            WebCore.Initialize(new WebCoreConfig { CustomCSS = "::-webkit-scrollbar { visibility: hidden; }" });
+        }
+
+        public static void Shutdown()
+        {
+            WebCore.Shutdown();
+        }
+    }
+}
