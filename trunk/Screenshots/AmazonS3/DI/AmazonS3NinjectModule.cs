a796b1ca4c423e96f0500fffdcff813412b6e1ed
alexidsa@gmail.com <alexidsa@gmail.com@708feb5c-fac0-84a1-9907-d073d775ba04>
2011-08-27 16:01:29 +0000

    Reorganized screenshots related projects

---
+++ trunk/Screenshots/AmazonS3/DI/AmazonS3NinjectModule.cs
@@ -0,0 +1,15 @@
+﻿using BetTeamsBattle.BettScreenshotsManager.BetScreenshotProcessor;
+using BetTeamsBattle.BettScreenshotsManager.BetScreenshotProcessor.Interfaces;
+using Ninject.Modules;
+
+namespace BetTeamsBattle.BettScreenshotsManager.DI
+{
+    public class AmazonS3NinjectModule : NinjectModule
+    {
+        public override void Load()
+        {
+            Bind<IBetScreenshotPathService>().To<BetScreenshotPathService>();
+            Bind<IScreenshotAmazonS3Putter>().To<ScreenshotAmazonS3Putter>();
+        }
+    }
+}
---
+++ trunk/Screenshots/AwesomiumScreenshotMaker/DI/AwesomiumScreenshotMakerNinjectModule.cs
@@ -0,0 +1,16 @@
+﻿using BetTeamsBattle.AwesomiumScreenshotMaker.Interfaces;
+using Ninject.Modules;
+
+namespace BetTeamsBattle.AwesomiumScreenshotMaker.DI
+{
+    public class AwesomiumScreenshotMakerNinjectModule : NinjectModule
+    {
+        public override void Load()
+        {
+            Bind<IRenderBufferToPngStreamConverter>().To<RenderBufferToPngStreamConverter>();
+            Bind<IScreenshotMaker>().To<ScreenshotMaker>();
+            Bind<IScreenshotMakerFactory>().To<ScreenshotMakerFactory>();
+            Bind<IScreenshotRenderService>().To<ScreenshotRenderService>();
+        }
+    }
+}
