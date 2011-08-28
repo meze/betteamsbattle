282ed96bea9b27a694d195be62d853ca9b473e64
alexidsa@gmail.com <alexidsa@gmail.com@708feb5c-fac0-84a1-9907-d073d775ba04>
2011-08-28 16:29:09 +0000

    Applied markup version 34

---
+++ trunk/Frontend/Models/Shared/FormElementViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Mvc;
+
+namespace BetTeamsBattle.Frontend.Models
+{
+    public class FormElementViewModel
+    {
+        public string Title { get; set; }
+        public MvcHtmlString Element { get; set; }
+        public MvcHtmlString ValidationMessage { get; set; }
+
+        public FormElementViewModel(string title, MvcHtmlString element, MvcHtmlString validationMessage)
+        {
+            Title = title;
+            Element = element;
+            ValidationMessage = validationMessage;
+        }
+    }
+}
