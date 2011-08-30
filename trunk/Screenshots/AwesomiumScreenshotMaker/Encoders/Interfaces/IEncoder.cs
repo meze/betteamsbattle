ad349f8f69ad9848e9cd6aee29b3a5073d1921e2
alexidsa@gmail.com <alexidsa@gmail.com@708feb5c-fac0-84a1-9907-d073d775ba04>
2011-08-30 07:40:26 +0000

    Refactored screenshots encoding

---
+++ trunk/Screenshots/AwesomiumScreenshotMaker/Encoders/Interfaces/IEncoder.cs
@@ -0,0 +1,9 @@
+﻿using System.Windows.Media.Imaging;
+
+namespace BetTeamsBattle.Screenshots.AwesomiumScreenshotMaker.Encoders.Interfaces
+{
+    internal interface IEncoder
+    {
+        BitmapEncoder GetEncoder(BitmapSource bitmap);
+    }
+}
---
+++ trunk/Screenshots/AwesomiumScreenshotMaker/Encoders/JpegEncoder.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Media.Imaging;
+using BetTeamsBattle.Screenshots.AwesomiumScreenshotMaker.Encoders.Interfaces;
+
+namespace BetTeamsBattle.Screenshots.AwesomiumScreenshotMaker.Encoders
+{
+    internal class JpegEncoder : IEncoder
+    {
+        public BitmapEncoder GetEncoder(BitmapSource bitmap)
+        {
+            var jpegEncoder = new JpegBitmapEncoder() {QualityLevel = 50};
+            jpegEncoder.Frames.Add(BitmapFrame.Create(bitmap));
+            return jpegEncoder;
+        }
+    }
+}
