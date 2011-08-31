bf5d65a16338756dd0b6ac125786331a90b80f8b
alexidsa <alexidsa@gmail.com>
2011-08-31 21:39:19 +0000

    SVN sync

---
+++ Frontend/Areas/NotAdmin/Models/BattleBets/StatusActionImageViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Mvc;
+
+namespace BetTeamsBattle.Frontend.Areas.NotAdmin.Models.BattleBets
+{
+    public class StatusActionImageViewModel
+    {
+        public ActionResult ActionResult { get; set; }
+        public string ImageClass { get; set; }
+        public string Title { get; set; }
+
+        public StatusActionImageViewModel(ActionResult actionResult, string imageClass, string title)
+        {
+            ActionResult = actionResult;
+            ImageClass = imageClass;
+            Title = title;
+        }
+    }
+}
