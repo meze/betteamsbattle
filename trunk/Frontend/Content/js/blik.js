028b45d51eb2b03332dc4273f1737650b5504b02
alexidsa@gmail.com <alexidsa@gmail.com@708feb5c-fac0-84a1-9907-d073d775ba04>
2011-09-03 12:32:50 +0000

    Made team choice hideable in the case of one userteam

--- trunk/Frontend/Content/js/blik.js
+++ trunk/Frontend/Content/js/blik.js
@@ -90,5 +90,26 @@ $(".knopka_ros").hover(
 
 
 
+    function setEqualHeight(columns)
+    {
+    var tallestcolumn = 0;
+    columns.each(
+    function()
+    {
+    currentHeight = $(this).height();
+    if(currentHeight > tallestcolumn)
+    {
+    tallestcolumn = currentHeight;
+    }
+    }
+    );
+    columns.height(tallestcolumn);
+    }
+    $(document).ready(function() {
+    setEqualHeight($(".content_blok_b > div"));
+    });
+
+
+
 
 }); //Конец ready
