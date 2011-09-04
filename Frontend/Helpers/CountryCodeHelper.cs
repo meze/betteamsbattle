2dbc0946e6b9a97c49ad56bf0aba64a9833d0fd0
alexidsa <alexidsa@gmail.com>
2011-09-04 11:51:16 +0000

    Implemented UI language support

---
+++ Frontend/Helpers/CountryCodeHelper.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using BetTeamsBattle.Data.Model.Enums;
+using BetTeamsBattle.Frontend.Localization.Infrastructure.LanguageMapping;
+
+namespace BetTeamsBattle.Frontend.Helpers
+{
+    public class CountryCodeHelper
+    {
+        public static string ChangeUrlCountryCodePart(Uri uri, Language language)
+        {
+            var path = uri.GetComponents(UriComponents.Path, UriFormat.Unescaped);
+
+            string pathCountryCode;
+            GetPathCountryCodeParts(path, out pathCountryCode);
+            if (LanguageMappingHelper.CountryCodeIsSupported(pathCountryCode))
+            {
+                path = path.Substring(2);
+                if (path == String.Empty)
+                    path = "/";
+            }
+
+            var countryCode = LanguageMappingHelper.GetCountryCodeByLanguage(language);
+
+            var uriBuilder = new UriBuilder(uri);
+            if (path == String.Empty)
+                uriBuilder.Path = countryCode;
+            else
+            {
+                if (!path.StartsWith("/"))
+                    path = "/" + path;
+                uriBuilder.Path = String.Format("{0}{1}", countryCode, path);
+            }
+
+            return uriBuilder.Uri.ToString();
+        }
+
+        public static void GetPathCountryCodeParts(string path, out string pathCountryCode)
+        {
+            pathCountryCode = String.Empty;
+
+            if (path.Length >= 2)
+                if (path.Length == 2 || path.Substring(2, 1) == "/")
+                    pathCountryCode = path.Substring(0, 2);
+        }
+    }
+}
---
+++ Frontend/Models/Shared/Languages/LanguageViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System.Web.Mvc;
+using BetTeamsBattle.Data.Model.Enums;
+using BetTeamsBattle.Frontend.Localization.Infrastructure;
+
+namespace BetTeamsBattle.Frontend.Models.Shared.Languages
+{
+    public class LanguageViewModel
+    {
+        public Language Language { get; set; }
+        public ActionResult ChangeLanguageActionResult { get; set; }
+        public string LanguageClass { get; set; }
+        public string CurrentLanguageClass { get; set; }
+
+        public bool IsCurrentLanguage
+        {
+            get { return Language == CurrentLanguage.Language; }
+        }
+
+        public LanguageViewModel(Language language, string languageClass, string currentLanguageClass)
+        {
+            Language = language;
+            LanguageClass = languageClass;
+            CurrentLanguageClass = currentLanguageClass;
+        }
+    }
+}
