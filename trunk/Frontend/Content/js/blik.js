ea092c3faf962501978d34c0ec0e3ac9f2952725
alexidsa@gmail.com <alexidsa@gmail.com@708feb5c-fac0-84a1-9907-d073d775ba04>
2011-09-20 19:28:48 +0000

    Applied markup version 73

--- trunk/Frontend/Content/js/blik.js
+++ trunk/Frontend/Content/js/blik.js
@@ -4,65 +4,7 @@ $(document).ready(function() {
     
 
 
-//---------------------------------------------------------------
-$(".knopka_1").hover(
-  function () {
-    $(".knopka_1_hover").show();
-  },
-  function () {
-    $(".knopka_1_hover").hide();
-  }
-);
 
-
-$(".knopka_2").hover(
-  function () {
-    $(".knopka_2_hover").show();
-  },
-  function () {
-    $(".knopka_2_hover").hide();
-  }
-);
-
-$(".knopka_3").hover(
-  function () {
-    $(".knopka_3_hover").show();
-  },
-  function () {
-    $(".knopka_3_hover").hide();
-  }
-);
-
-
-$(".knopka_4").hover(
-  function () {
-    $(".knopka_4_hover").show();
-  },
-  function () {
-    $(".knopka_4_hover").hide();
-  }
-);
-
-
-$(".knopka_eng").hover(
-  function () {
-    $(".knopka_eng_hover").show();
-  },
-  function () {
-    $(".knopka_eng_hover").hide();
-  }
-);
-
-
-
-$(".knopka_ros").hover(
-  function () {
-    $(".knopka_ros_hover").show();
-  },
-  function () {
-    $(".knopka_ros_hover").hide();
-  }
-);
 
     function setEqualHeight(columns)
     {
@@ -82,5 +24,17 @@ $(".knopka_ros").hover(
     $(document).ready(function() {
     setEqualHeight($(".content_blok > div, .content_blok_b > div"));
     });
+
+
+
+
+
+
+
+
+ $(".knopka_st").hover(function () {
+      $(this).toggleClass("ff");
+    });
+
 
 }); //Конец ready
